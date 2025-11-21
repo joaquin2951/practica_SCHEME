e34a6254528b6d40cca5fc835a3b4800d108e8cd
Joaquin Hernandez <joaquin.hernandezutn@gmail.com>
2025-11-21 18:26:59 -0300

    21-NOV-2025
    FINALIZACION GUIA 2

--- ejercicios/guia_2.rkt
+++ ejercicios/guia_2.rkt
@@ -98,5 +98,155 @@
 
 ;31. Se le solicita que proponga las funciones Scheme PUTPROP y GETPROP.
 
+(define base '((juan edad 30) (juan direccion (lavaise 610)) (pedro edad 34) (pedro hijos 
+2) (pedro sueldo 1400)))
 
 
+;GETPROP obtiene el valor para un conjunto (objeto atributo)
+;ejemplo:  (GETPROP base 'juan 'edad)  --> 30
+;las listas tienen la forma '(OBJETO ATRIBUTO VALOR)
+(define obj
+  (lambda(lst)
+    (car lst)))
+
+(define atr
+  (lambda(lst)
+    (cadr lst)))
+
+(define (val lst)
+  (caddr lst)
+  )
+
+(define GETPROP
+  (lambda (BaseDeDatos Objeto Atributo)
+    (cond
+      ((null? BaseDeDatos) -1)
+
+      ( (eqv? (and (equal? (obj (car BaseDeDatos)) Objeto) (equal? (atr (car BaseDeDatos)) Atributo)) #t)
+       (val (car BaseDeDatos)))
+
+      (else
+       (GETPROP (cdr BaseDeDatos) Objeto Atributo)))))
+
+
+
+(define PUTPROP 
+(lambda (BasedeDatos Objeto Atributo Valor)
+  (if (equal? (GETPROP BasedeDatos Objeto Atributo) -1) (cons (list Objeto Atributo Valor) BasedeDatos)
+      #f);deberiamos cambiar el retorno de #f por un camino en el que hagamos eliminar la terna (Objeto Atributo X) de la lista y entonces luego si hacer
+         ;el CONS de la terna nueva al principio de la lista
+)
+)
+
+
+
+
+;35. Uno de los recorridos de árboles es en forma horizontal, el cual consiste en visitar los 
+;nodos que están en un mismo nivel (de izquierda a derecha), luego proceder con los nodos 
+;del próximo nivel y así sucesivamente hasta visitar todos los nodos del árbol.
+;
+;FORMA DE ARBOL = '(RAIZ AI AD)
+(define arbol35
+  '(a (c
+        (t () () )
+        (r
+           (q () ())
+           (z () () )) )
+      (j
+         (h () () )
+         (m () () )) )
+)
+
+;el proceso podría pensarse a partir de una lista de árboles, y a medida que se extrae 
+;una raíz, los árboles generados se incorporan al final de la lista.
+
+(define (recorridoHorizontal lstArboles)
+  (cond
+      ( (null? lstArboles) '() )
+      ( (esVacio? (car lstArboles)) (recorridoHorizontal (cdr lstArboles)) )
+      ;( (esHoja?  (car lstArboles)) (list (Raiz (car lstArboles))))  NO HACE FALTA PENSAR ESTA CONDICION, SE SATISFACE CON LA DE ABAJO
+      ( else (append (list (Raiz (car lstArboles))) (recorridoHorizontal (append (cdr lstArboles) (list (AI (car lstArboles))) (list (AD (car lstArboles))) )) ) )
+   )
+ )
+
+;PRUEBAS:
+; (recorridoHorizontal (list arbol35))
+; (recorridoHorizontal (list arbol1))
+; (recorridoHorizontal (list arbol2))
+; (recorridoHorizontal (list arbol3))
+; (recorridoHorizontal (list arbol4))
+
+
+
+
+;38. Se le solicita que defina la siguiente función en Scheme:
+;en donde ListaFunciones es una lista de funciones de un argumento. La aplicación de 
+;mapeoFunciones aplica todas las funciones de ListaFunciones a los elementos de Lista, 
+;retornando una lista con los resultados de estas evaluaciones.
+
+(define suma1 (lambda(x) (+ 1 x)))
+(define resta1 (lambda(x) (- x 1)))
+(define por2 (lambda(x) (* x 2)))
+(define lstFunct (list suma1 resta1 por2))
+
+(define evaluarFunciones
+  (lambda(ListaFunciones Elemento)
+    (if (null? ListaFunciones) '()
+        (append (list ( (car ListaFunciones) Elemento )) (evaluarFunciones (cdr ListaFunciones) Elemento) )
+    )))
+
+(define mapeoFunciones
+  (lambda(ListaFunciones Lista)
+     (if (null? Lista) Lista
+        (append (list (evaluarFunciones ListaFunciones (car Lista))) (mapeoFunciones ListaFunciones (cdr Lista)))
+  )))
+
+
+
+
+;42. Se quiere calcular las comisiones de viajantes de una empresa. Las mismas depende de los 
+;montos vendidos por cada viajante y del producto que vendió. Para ello se cuenta con la 
+;información de las ventas por producto que realizó cada viajante.
+
+;la información sobre viajantes está organizada de la siguiente manera:
+;((viajante1(venta,productoA)(vta,productoB)....) (viajante2(venta, productoC)(venta,ProductoA)..)...) 
+;y la información sobre los productos está organizado en otra lista de la siguiente manera: 
+;( (productoA, porcentajeA) (produtoB, porcentajeB).......)
+
+;El resultado de la evaluación de la función que debe crear, debe ser la lista de los viajantes 
+;a la que se le agrega un dato más al final que es su comisión. De esta manera la lista 
+;resultante será: 
+;((viajante1(vta prodA)(vta prodB)..... comisión)(....))
+(define Productos
+  '( (productoA 10) (productoB 10) (productoC 0) (productoD 100)  )
+  )
+(define Viajantes
+  '(
+    (viajante1 (10 productoA) (10 productoB) (0 productoC))
+    (viajante2 (10 productoA) (10 productoB) (10 productoC) (10 productoD) )
+    (viajante3 (10 productoA) (10 productoB) (10 productoC) (0 productoD) )
+    )
+ )
+
+(define porcentajeProducto
+  (lambda(Producto lstProd)
+    (cond
+      ( (null? lstProd) 0)
+      ( (equal? (caar lstProd) Producto) (cadar lstProd))
+      ( else (porcentajeProducto Producto (cdr lstProd)) )
+     )))
+
+(define calculaComisionViajante
+  (lambda(viajante lstProductos) ;recibe una lista de forma ( (10 prodA) (15 prodB) ... etc)
+    (if (null? viajante) 0
+         (+ (+ (caar viajante) (porcentajeProducto (cadar viajante) lstProductos)) (calculaComisionViajante (cdr viajante) lstProductos))
+         )))
+    
+
+(define listaComisiones
+  (lambda(lstViajantes lstProductos)
+    (if (null? lstViajantes) '()
+         (cons (append (car lstViajantes) (list (calculaComisionViajante (cdar lstViajantes) lstProductos)) ) (listaComisiones (cdr lstViajantes) lstProductos))
+     )))
+        
+  
